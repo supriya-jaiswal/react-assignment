a4d9f21eb41ac976176d78e0762a131a8da50197
unknown <jaiswalsupriya0611@gmail.com>
2020-05-24 19:16:26 +0530

    my first commit

--- backend/app.js
+++ backend/app.js
@@ -13,5 +13,6 @@ var server = app.listen(5000, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at port", port)
+   
 })
